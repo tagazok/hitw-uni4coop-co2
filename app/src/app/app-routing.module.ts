8e8bb627f10f66782995c0e2131909b1a5e5c3a7
tagazok <leplus.olivier@gmail.com>
2022-06-24 21:59:59 +0200

    fix(typo): Fix typos in routes

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './dashboard/home/home.component';
 import { TripAddComponent } from './dashboard/trip-add/trip-add.component';
-import { TripComponent } from './dashboard/trip/trip.component';
 import { LoginComponent } from './login/login.component';
 import { TripInfoComponent } from './dashboard/trip-info/trip-info.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: HomeComponent },
   { path: 'dashboard/trips/add', component: TripAddComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard/trips/:id', component: TripInfoComponent },
 ];
