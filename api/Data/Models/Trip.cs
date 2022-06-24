65c04740d7da8444429081625189df7c663cc2aa
Titouan CREACH <t.creach@unitee.io>
2022-06-24 19:57:35 +0200

    feat(api): generate models

---
+++ api/Data/Models/Trip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data.Models
+{
+    public partial class Trip
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(256)]
+        public string Label { get; set; }
+        [StringLength(256)]
+        public string Departure { get; set; }
+        [StringLength(256)]
+        public string Arrival { get; set; }
+        public bool? IsRoundTrip { get; set; }
+        public int? UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        [InverseProperty("Trips")]
+        public virtual User User { get; set; }
+    }
+}
