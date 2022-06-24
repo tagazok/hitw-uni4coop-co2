fcedf1fb2895324ad867c4ac886c3d8f0d6ff502
Titouan CREACH <t.creach@unitee.io>
2022-06-24 23:12:40 +0200

    wip

--- api/Trips.cs
+++ api/Trips.cs
@@ -12,9 +12,16 @@ using System.Linq;
 using System.Security.Claims;
 using System.Net;
 using api.Data.Models;
+using Flurl.Http;
 
 namespace HITW.Function
 {
+    public class ClimatiqResp
+    {
+        [JsonProperty("co2e")]
+        public double Co2e { get; set; }
+    }
+
     public class Trips
     {
         private readonly HITWDbContext _dbContext;
@@ -35,9 +42,19 @@ namespace HITW.Function
             {
                 return new StatusCodeResult((int)HttpStatusCode.Unauthorized);
             }
-        
+
             var body = JsonConvert.DeserializeObject<Trip>(await new StreamReader(req.Body).ReadToEndAsync());
-        
+
+            var resp = await "https://beta3.api.climatiq.io/travel/flights"
+                .WithOAuthBearerToken("XNVBAM6P1E46RQGPBXNWDAC9B916")
+                .PostJsonAsync(new
+                {
+                    from = body.Departure,
+                    to = body.Arrival,
+                    passengers = 1,
+                    class = "economy"
+                }).GetJson<ClimatiqResp>();
+
             var trip = new Trip
             {
                 Label = body.Label,
@@ -49,7 +66,7 @@ namespace HITW.Function
 
             _dbContext.Trips.Add(trip);
             _dbContext.SaveChanges();
-        
+
             return new OkObjectResult(_dbContext.Trips.Where(x => x.UserId == user.Id).Select(x => new
             {
                 label = x.Label,
@@ -70,7 +87,7 @@ namespace HITW.Function
             var u = StaticWebAppAuth.Parse(req);
             var externalId = u.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
             var user = _dbContext.Users.Where(x => x.ExternalId == externalId).SingleOrDefault();
-            
+
             if (user is null)
             {
                 return new StatusCodeResult((int)HttpStatusCode.Unauthorized);
