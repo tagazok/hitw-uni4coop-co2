f09cafc94a34eb83a98031d73931d71c28cafe76
Nathan Pire <nathan.pire@gmail.com>
2022-06-24 18:16:25 +0200

    generating dashboard-home,-tripList,-tripAdd

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './dashboard/home/home.component';
+import { TripListComponent } from './dashboard/trip-list/trip-list.component';
+import { TripAddComponent } from './dashboard/trip-add/trip-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TripListComponent,
+    TripAddComponent
   ],
   imports: [
     BrowserModule,
