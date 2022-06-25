a2d21966c1e316ff529cf703b1f75df822a1a88e
Nathan Pire <nathan.pire@gmail.com>
2022-06-25 13:20:14 +0200

    making the function to send a reward

--- app/src/app/dashboard/activity-add/activity-add.component.ts
+++ app/src/app/dashboard/activity-add/activity-add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Code } from 'src/app/models/reward';
+import { RewardService } from 'src/app/services/reward.service';
+import { Reward } from 'src/app/models/reward';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-activity-add',
@@ -6,10 +11,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activity-add.component.scss']
 })
 export class ActivityAddComponent implements OnInit {
+  private tripId: number | undefined;
 
-  constructor() { }
+  constructor(
+    private rewardService: RewardService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.tripId = +this.activatedRoute.snapshot.params['tripId']
+  }
+  sendReward(code: Code) {
+    switch (code) {
+      case Code.VEGGIE:
+      case Code.SHOWER:
+      case Code.PLASTIC:
+      case Code.COMPUTER:
+      case Code.THERMOSTAT:
+        let reward: Reward = {
+          code: code,
+          tripId: this.tripId!,
+          distance: undefined,
+        }
+        this.rewardService.addReward(reward).subscribe(
+          (receivedReward) => {
+            console.log(receivedReward)
+          }
+        )
+        break;
+      case Code.TRANSPORTATION:
+        break;
+
+    }
   }
 
 }
--- app/src/app/models/reward.ts
+++ app/src/app/models/reward.ts
@@ -1,14 +1,13 @@
 export interface Reward {
   code: Code;
-  distance: number;
+  distance: number | undefined;
   tripId: number;
 }
 export enum Code {
-  VEGAN = 1,
-  TRANSPORT = 2,
-  SHOWER = 3,
-  BAG = 4,
-  RECYLCING = 5,
-  TURNOFF = 6,
-  DEGREE = 7,
+  VEGGIE = "VEGGIE",
+  TRANSPORTATION = "TRANSPORTATION",
+  SHOWER = "SHOWER",
+  PLASTIC = "PLASTIC",
+  COMPUTER = "COMPUTER",
+  THERMOSTAT = "THERMOSTAT",
 }
