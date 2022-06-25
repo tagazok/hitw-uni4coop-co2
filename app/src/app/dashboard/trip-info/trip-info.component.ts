19225b510b13706e58292ededf42f4eacb4bf67f
Amagash <tiffanysouterre@gmail.com>
2022-06-25 18:19:41 +0200

    update thermostat sentence

--- app/src/app/dashboard/trip-info/trip-info.component.ts
+++ app/src/app/dashboard/trip-info/trip-info.component.ts
@@ -34,7 +34,7 @@ export class TripInfoComponent implements OnInit {
       `This travel represents ${((this.trip?.co2 || 0) / 7).toFixed()} steaks of 200g`,
       `To compensate this travel, you should take ${((this.trip?.co2 || 0) / 1.06).toFixed()} showers instead of taking baths`,
       `To compensate this travel, you should reuse ${((this.trip?.co2 || 0) / 0.2).toFixed()} times a bag instead of buying a plastic bag`,
-      `To compensate this travel, you should turn down your thermostat by 1°C for ${((this.trip?.co2 || 0)).toFixed()} days`,
+      `To compensate this travel, you should turn down your thermostat by 1°C lower than usual for ${((this.trip?.co2 || 0)).toFixed()} days`,
       `To compensate this travel, you should turn off your computer ${((this.trip?.co2 || 0) / 0.2).toFixed()} days instead of using battery saver`
     ];
 
